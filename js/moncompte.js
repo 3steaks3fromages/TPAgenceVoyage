8950d4b884db793b1ed03ad02f79f8a38b436689
Timothee Levilly <tim.levilly@hotmail.fr>
2020-12-10 22:25:39 +0100

    moncompte 2

--- js/moncompte.js
+++ js/moncompte.js
@@ -1,4 +1,9 @@
 function initialisation(){
     verifConnexion();
-    
+    email=getCookie("email");
+    nom=getCookie("nom");
+    prenom=getCookie("prenom");
+    document.getElementById("email").innerHTML=email;
+    document.getElementById("nom").innerHTML=nom;
+    document.getElementById("prenom").innerHTML=prenom;
 }
